fd3b9f88c2ae677f7354e2b0f927b85771aa5a23
elbertchi <alberto.garcia@homyspace.com>
2020-12-06 22:34:08 +0100

    6.1 que no me acepta el resultado

--- index.js
+++ index.js
@@ -10,8 +10,12 @@ try {
     console.log('Error:', e.stack);
 }
 
+
+
 return data.toString();
 }
+
+const inputData = files()
 
 const getUniqueAnswers = function(str){
   var setOfAnswers = new Set();
@@ -33,9 +37,41 @@ const countValues = function(grSet){
 const getAllPassengers = files()
 .then(res => res
   .split('\r\n\r\n')
-  .map(group=>countValues(group.replace(/\r|\n/gm,''))
+  .map(group=>countValues(group.replace(/\r\n|\n|\r/gm,''))
   ))
 
 .then(p=>console.log(counts + ' nº of counts'))
 
 
+
+/*  
+
+function getAnswersByGroup(input) {
+  return input.split('\r\n\r\n');
+}
+
+function getUniqueAnswersByGroup(groups) {
+  return groups.map(group => {
+    group = group.replace(/\n|\r/g, "");
+    return [...new Set(group)]
+  });
+}
+
+function getSumOfUniqueAnswerCountByGroup(groups) {
+  return groups.reduce((acc, group) => {
+    return acc += group.length
+  }, 0);
+}
+
+async function solve(input) {
+  
+  const groups = getAnswersByGroup(input);
+  const uniqueAnswersByGroup = getUniqueAnswersByGroup(groups)
+ 
+  return console.log(getSumOfUniqueAnswerCountByGroup(uniqueAnswersByGroup));
+}
+
+const getResult = files().then(r=> solve(r))
+
+
+*/
