463219e82881bd3465f58c4adf68e664fad99b18
elbertchi <alberto.garcia@homyspace.com>
2020-12-07 00:09:39 +0100

    6.2 + nodemon

--- index.js
+++ index.js
@@ -4,18 +4,15 @@ var counts = 0;
 
 const files = async function() {
     var data;
-try {  
-    data = await fs.readFileSync('input4.txt', 'utf8');
-} catch(e) {
-    console.log('Error:', e.stack);
-}
-
-
-
+  try {  
+      data = await fs.readFileSync('input4.txt', 'utf8');
+  } catch(e) {
+      console.log('Error:', e.stack);
+  }
 return data.toString();
 }
 
-const inputData = files()
+
 
 const getUniqueAnswers = function(str){
   var setOfAnswers = new Set();
@@ -25,21 +22,47 @@ const getUniqueAnswers = function(str){
   return setOfAnswers;
 } 
 
+
 const countValues = function(grSet){
-  var values = getUniqueAnswers(grSet)
-  counts += values.size;
-  console.log(values)
-  console.log('size ' + values.size)
+  var uniqueValues = getUniqueAnswers(grSet.replace(/\r\n|\n|\r/gm,''))
+  var uniqueValIterable = [...uniqueValues];
+  var groupAnswers = grSet.split('\n');
+  console.log(uniqueValIterable)
+  console.log(groupAnswers)
+
+
+  for (const letter of uniqueValIterable){
+
+    var coincidences = 0;
+    groupAnswers.map(personAnswers => {
+
+      if (personAnswers.includes(letter)) {
+        coincidences +=1;
+        
+        }
+      })
+    
+    if (coincidences == groupAnswers.length) {
+     
+      counts +=1
+      continue;
+    
+    };
+
+  }
+
+ 
   return true;
 
 }
 
-const getAllPassengers = files()
+const run = files()
 .then(res => res
   .split('\n\n')
-  .map(group=>countValues(group.replace(/\r\n|\n|\r/gm,''))
-  ))
+  .map(rawGroups=> countValues(rawGroups))
+  //.map(group=>countValues(group.replace(/\r\n|\n|\r/gm,''))
+  )
 
-.then(p=>console.log(counts + ' nº of counts'))
+.then(p=>console.log(counts))
 
 
