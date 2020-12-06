121653e77982537914fdea6b33f2ce402c728b49
elbertchi <alberto.garcia@homyspace.com>
2020-12-06 22:50:32 +0100

    advent 6.1 funcionando, fallaba el input

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var counts = 0;
 const files = async function() {
     var data;
 try {  
-    data = await fs.readFileSync('input.txt', 'utf8');
+    data = await fs.readFileSync('input4.txt', 'utf8');
 } catch(e) {
     console.log('Error:', e.stack);
 }
@@ -36,42 +36,10 @@ const countValues = function(grSet){
 
 const getAllPassengers = files()
 .then(res => res
-  .split('\r\n\r\n')
+  .split('\n\n')
   .map(group=>countValues(group.replace(/\r\n|\n|\r/gm,''))
   ))
 
 .then(p=>console.log(counts + ' nº of counts'))
 
 
-
-/*  
-
-function getAnswersByGroup(input) {
-  return input.split('\r\n\r\n');
-}
-
-function getUniqueAnswersByGroup(groups) {
-  return groups.map(group => {
-    group = group.replace(/\n|\r/g, "");
-    return [...new Set(group)]
-  });
-}
-
-function getSumOfUniqueAnswerCountByGroup(groups) {
-  return groups.reduce((acc, group) => {
-    return acc += group.length
-  }, 0);
-}
-
-async function solve(input) {
-  
-  const groups = getAnswersByGroup(input);
-  const uniqueAnswersByGroup = getUniqueAnswersByGroup(groups)
- 
-  return console.log(getSumOfUniqueAnswerCountByGroup(uniqueAnswersByGroup));
-}
-
-const getResult = files().then(r=> solve(r))
-
-
-*/
