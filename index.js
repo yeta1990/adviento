9cc1e025792fb39dcd00b91ffce81ff2773e182d
elbertchi <alberto.garcia@homyspace.com>
2020-12-08 01:40:19 +0100

    7.1

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-var fs = require('fs');
 
-var counts = 0;
+var fs = require('fs');
+const { get } = require('http');
+
 
 const files = async function() {
     var data;
@@ -10,35 +11,58 @@ try {
     console.log('Error:', e.stack);
 }
 
-
-
 return data.toString();
 }
 
+const createMainBags = function (input) {
 
-const getUniqueAnswers = function(str){
-  var setOfAnswers = new Set();
-  for (letter of str) {
-    setOfAnswers.add(letter);
-  }
-  return setOfAnswers;
-} 
+  var allBags = []
+  input.map(line=> {
+  
+    var key = line.match(/\w*\s\w*/)[0]
+    var mainBag = {bag: key, contains: []}
+    var re = /\d\s(\w*\s\w*)?/gm;
 
-const countValues = function(grSet){
-  var values = getUniqueAnswers(grSet)
-  counts += values.size;
-  console.log(values)
-  console.log('size ' + values.size)
-  return true;
+    try {
+
+      var match; 
+
+      while ((match = re.exec(line)) !== null) {
+       
+        mainBag.contains.push(match[1])
+      }
+      
+      console.log(mainBag)
+    } catch (error) {
+      
+    }
+   
+    console.log(key)
+    allBags.push(mainBag)
+    
+  })
+
+  return allBags;
+}
+
+var results = new Set();
+
+const searchContainers = function(bags, objective){
+  
+  const allbags = bags;
+  console.log('buscando '+ objective)
+  allbags.forEach(element => {
+    if(element.contains.includes(objective)){
+      results.add(element.bag);
+      return searchContainers(allbags, element.bag)
+    }
+  });
 
 }
 
 const run = files()
-.then(res => res
-  .split('\r\n\r\n')
-  .map(group=>countValues(group.replace(/\r\n|\n|\r/gm,''))
-  ))
-
-.then(p=>console.log(counts + ' nº of counts'))
+.then(res => createMainBags(res.split('\n')))
+.then(mainBags => searchContainers(mainBags, 'shiny gold'))
+.then(p=>console.log('shiny gold puede ir en un total de  ' + results.size + ' bags distintos'))
 
 
