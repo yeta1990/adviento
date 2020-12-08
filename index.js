ebadb2eab8b980c62064d68ea539fee46be14983
elbertchi <alberto.garcia@homyspace.com>
2020-12-08 03:31:47 +0100

    3.2

--- index.js
+++ index.js
@@ -27,17 +27,16 @@ const createMainBags = function (input) {
       var match; 
 
       while ((match = re.exec(line)) !== null) {
-       
+      
         mainBag.contains.push({bag: match[2], stock: parseInt(match[1])})
-        //mainBag.stock += parseInt(match[1])
+      
       }
       
-       //console.log(mainBag)
+      
     } catch (error) {
       
     }
    
-    // console.log(key)
     allBags.push(mainBag)
     
   })
@@ -50,39 +49,24 @@ var results = 0;
 const searchContainers = function(bags, objective, stock){
   
   const allbags = bags;
-  //console.log('buscando '+ objective)
+ 
   allbags.forEach(element => {
     if(element.bag == objective){
-      console.log(element)
+      
       try {
-        var multiply;
-        
-
         element.contains.forEach(singleBag => {
-          console.log('buscando ' + singleBag.stock + ' ' + singleBag.bag )
-          //console.log(stock)
-          //console.log(singleBag.stock*stock)
-          
-
           var toSum = singleBag.stock*stock;
-          console.log(toSum)
-          
           if (toSum != 0){
-            console.log('sumo ' + singleBag.stock + '*' + stock)
             results += toSum;
           } else {
-            console.log('sumo ' + singleBag.stock)
             toSum = singleBag.stock;
             results += singleBag.stock;
-          }
-         
+          }         
           searchContainers(allbags, singleBag.bag, toSum)
-          
         })
-        
-        //return searchContainers(allbags, element.bag)
+       
       } catch (error) {
-        //console.error(error)
+        
       }
     
       return ;
@@ -94,6 +78,6 @@ const searchContainers = function(bags, objective, stock){
 const run = files()
 .then(res => createMainBags(res.split('\n')))
 .then(mainBags => searchContainers(mainBags, 'shiny gold', 0))
-.then(p=>console.log('shiny gold puede ir en un total de  ' + results + ' bags distintos'))
+.then(p=>console.log('shiny gold puede contener en un total de  ' + results + ' bags distintos'))
 
 
