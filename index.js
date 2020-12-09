02becb8950fb3ce01f48452bc988d1d4a1ea6ce6
elbertchi <alberto.garcia@homyspace.com>
2020-12-09 23:42:10 +0100

    9.1

--- index.js
+++ index.js
@@ -10,18 +10,48 @@ try {
 return data.toString().split('\n');
 }
 
-const calc = function(expenses) {
+const calc = function(xmas) {
 
-  const exp = expenses.map(ex => parseInt(ex));
-  console.log(exp)
-  exp.map(line => exp.map(f=> {
+  const exp = xmas.map(ex => parseInt(ex));
+  
+  
+  for (i=0; i<exp.length; i++){
 
-    if ((f+line) == 2020){
-      console.log(f*line)
-      
-    }}
-  ))
+    const subset = exp.slice(i, i+26)
+    var found = false;
+
+    for (j=0; j<25;j++){
+
+      for (h=0; h<25;h++){
+        if (subset[j] + subset[h] == subset[25]) {
+          
+          found = true
+          console.log(subset[25])
+        };
+      }
+
+
+    }
+
+    // solución que no me ha funcionado :(
+    /*subset.map(line => 
+      {
+        
+        subset.map(f=> {
+        console.log(f + ' + ' + line + ' = ' + (f+line) + ' vs ' + exp[i+26])
+        if (((f+line) == exp[i+26]) ){
+          found = true
+        console.log('encontrada suma ' + f + ' + ' + line +  ' ' + exp[i+26] + ' en posición ' + (i+26) )
+        
+      }}
+    )}
+    )*/
+
+    if (!found) {console.log('falla el número ' + subset[25] + ' en posición ' + (25)); break;};
+
+  }
+
 }
 
-const run = files().then(expenses => calc(expenses)
+const run = files().then(xmas => calc(xmas)
 )
