21feeaead1c23d9aed5555365b2656ce66668390
elbertchi <alberto.garcia@homyspace.com>
2020-12-10 01:27:08 +0100

    9.2 optimizado

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+
+console.time("time")
 var fs = require('fs');
 
 const files = async function() {
@@ -10,13 +12,12 @@ try {
 return data.toString().split('\n');
 }
 
-
-
 const calc = function(xmas) {
 
   const exp = xmas.map(ex => parseInt(ex));
   
   var invalid = 0;
+  //dance:
   for (i=0; i<exp.length; i++){
 
     const subset = exp.slice(i, i+26)
@@ -28,52 +29,48 @@ const calc = function(xmas) {
         if (subset[j] + subset[h] == subset[25]) {
           
           found = true
-          
+          break //dance;
         };
       }
 
-
     }
-
-    if (!found) {console.log('falla el número ' + subset[25] + ' en posición ' + (25)); invalid = subset[25]; break;};
-
+    if (!found) {console.log('9.1: ' + subset[25]); invalid = subset[25]; break; };
   }
+  
+  console.timeEnd("time")
   return {invalid, exp};
 }
 
 const findContiguous = function(exp, invalid){
-  
+  console.time("time 2")
 
-  for (i=0; i<exp.length; i++){
-
-    const subset = exp.slice(i, i+25)
+  dance:
+  for(i=0; i<exp.length;i++) {
+    
     var found = false;
-
-    for (j=0; j<25;j++){
-      if (found) break;
-      
+    
       for (h=0; h<25;h++){
-        if (found) break;
-          const reducer = (accumulator, currentValue) => accumulator + currentValue;
+       
+        const reducer = (accumulator, currentValue) => accumulator + currentValue;
         
-          try {
-            if (subset.slice(j, h+1).reduce(reducer) == invalid){
-              found = true;
-              var combination = [...subset.slice(j, h+1)];
-           
-              console.log('encontrado ' + (Math.min(...combination) + Math.max(...combination)));
-              
-            }
-          } catch (error) {
+        try {
+          if (exp.slice(i, (i+h+1)).reduce(reducer) == invalid){
+            found = true;
+            var combination = [...exp.slice(i, (i+h+1))];
+         
+            console.log('9.2: ' + (Math.min(...combination) + Math.max(...combination)));
+            if (found) break dance;
             
-          } 
-
-
+          }
+        } catch (error) {
+          
+        }
+         
       }
-      
-    }
-    if (found) break;
+    
+     
   }
+  console.timeEnd("time 2")
 }
 
 
